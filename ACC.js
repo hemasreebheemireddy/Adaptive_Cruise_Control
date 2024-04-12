b8ac7fa2ceb0bb3583bd27a39f2b073c859e3a8b
hemasreebheemireddy <157704506+hemasreebheemireddy@users.noreply.github.com>
2024-04-12 15:48:03 +0530

    #1226 ACC.js
    Define criteria for automatic activation and deactivation of ACC.

---
+++ ACC.js
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h1>Adaptive Cruise Control</h1>
+  <script>document.write("[TASK-1226] Define criteria for automatic activation and deactivation of ACC")</script>
+</body>
+</html>
+ 
