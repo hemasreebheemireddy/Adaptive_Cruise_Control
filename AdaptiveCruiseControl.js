5003891b03ef3ebf5b5a78a4235c0ff4c1bf0194
hemasreebheemireddy <157704506+hemasreebheemireddy@users.noreply.github.com>
2024-04-12 15:52:32 +0530

    #1229 AdaptiveCruiseControl.js
    Integrate ACC functionality with the vehicle's onboard systems and controls, ensuring compatibility and interoperability.

---
+++ AdaptiveCruiseControl.js
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h1>Adaptive Cruise Control</h1>
+  <script>document.write("[TASK-1229] Integrate ACC functionality with the vehicle's onboard systems and controls, ensuring compatibility and interoperability
+")</script>
+</body>
+</html>
+ 
